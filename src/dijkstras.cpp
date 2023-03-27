3d03724f966b850a4ba61f8003bbe96a3e23739b
JCHUTK <chr8227@gmail.com>
2023-03-27 13:16:41 -0400

    Update dijkstras.cpp

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -1,8 +1,36 @@
 // dijsktras.cpp
-
 // Main Execution
+#include <iostream>
+#include <vector>
+#include <map>
+using namespace std;
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
+    map <char, int> path;
+    vector < vector <int> > vec;
+    int num_title, title_point, row, col, size;
+    char type, c;
+
+    cin >> num_title;
+    for (int i = 0; i < num_title; i++)
+    {
+        cin >> type >> title_point;
+        path.insert(pair<char, int>(type, title_point));
+    }
+
+    cin >> row >> col;
+
+    for (int i = 0; i < row; i++)
+    {
+        vec[i].resize(col);
+        for (int j = 0; j < col; j++)
+        {
+            cin >> c;
+            vec[i][j] = path.find(c)->second;
+        }
+    }
+
+
     return 0;
 }
-
